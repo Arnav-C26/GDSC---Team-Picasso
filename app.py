37083e393c18fe38f3c76279067a7b1572a850ad
ishantgandhi <126364217+ikenakami@users.noreply.github.com>
2024-05-15 18:18:34 -0700

    removed backend testing

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-# from flask import Flask, render_template
+from flask import Flask, render_template
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/')
-# def home():
-#     return render_template('home.html')
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-# @app.route('/generate')
-# def generate():
-#     return render_template('generator.html')
+@app.route('/generate')
+def generate():
+    return render_template('generator.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
